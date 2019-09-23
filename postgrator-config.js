882def493a6dd73deedfc757d6ba7a4651d108b8
Audreycious <porcupine4@gmail.com>
2019-09-23 18:37:26 -0500

    updates for breakfix

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,10 +6,5 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS,
   "ssl": !!process.env.SSL,
 }
