131e71d1cc53bfe35f4f918770dff136b576ecce
Audreycious <porcupine4@gmail.com>
2019-09-23 22:15:54 -0500

    updated to loginservice

--- src/login/login-router.js
+++ src/login/login-router.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const loginRouter = express.Router()
 const jsonBodyParser = express.json()
+const LoginService = require('./login-service')
+const logger = require('../logger')
 
 loginRouter
     .post('/', jsonBodyParser, (req, res, next) => {
@@ -10,22 +12,25 @@ loginRouter
         let { username, password } = req.body
         logger.info(username)
         logger.info(password)
-        knexInstance
-            .from('users')
-            .select('*')
-            .where({ username: username })
+        LoginService.getUser(knexInstance, username)
             .then(users => {
                 logger.info(users)
                 let user = users[0]
                 if (!user) {
                     return res.status(400).json({ error: `Username not found`})
                 }
-                else if (user.password !== password) {
-                    logger.error(`Invalid password entered for user: ${user.username}`)
-                    return res.status(401).json({ error: `Invalid password entered for user: ${user.username}` })
-                }
-                return res.status(200).json(`${user.username} is now logged in`)
+                // return LoginService.comparePasswords(user.password, password)
+                    // .then(passwordsMatch => {
+                        let passwordsMatch = user.password === password
+                        if (!passwordsMatch) {
+                            logger.error(`Invalid password entered for user: ${user.username}`)
+                            return res.status(401).json({ error: `Invalid password entered for user: ${user.username}` })
+                        }
+                        return res.status(200).json(`${user.username} is now logged in`)
+                    // })
+                
             })
+            .catch(next)
     })
 
 module.exports = loginRouter
--- src/login/login-service.js
+++ src/login/login-service.js
@@ -1,5 +1,16 @@
 
+const bcrypt = require('bcryptjs')
 
 const LoginService = {
-    
+    getUser(knex, username) {
+        return knex
+            .from('users')
+            .select('*')
+            .where({ username: username })
+    }, 
+    comparePasswords(password, hash) {
+        return bcrypt.compare(password, hash)
+    },
 }
+
+module.exports = LoginService
